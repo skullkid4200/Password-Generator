f9648c4a74d39253563b9369d3c6af8dcd51bf9c
skullkid4200 <jackeinhorn123@gmail.com>
2022-12-12 15:44:40 -0500

    pushing the final version of the code with all the functions. still have to add readME

--- Develop/script.js
+++ Develop/script.js
@@ -1,42 +1,54 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-const result = document.querySelector("#result");
-const includeNumbers = document.querySelector("#numbers");
-const includeSymbols = document.querySelector("#symbols");
+var result = document.querySelector("#result");
+
 
 var lowercaseLetters = "qwertyuiopasdfghjklzxcvbnm"
-var numbersIncluded = "1234567890"
+var numbers = "1234567890"
 var specialCharacters = "~`!@#$%^&*()-_=+':;,<.>/?"
 var upperCase = "QWERTYUIOPASDFGHJKLZXCVBNM"
-var generatedPassword = "";
+
 
 
 function generatePassword() {
 
-  var generatedPassword = document.querySelector("#password");
-  generatedPassword.value = password;
-
+  var generatedPassword = "";
   var passwordLength = window.prompt("Enter Password Length (Between 8 and 128 Characters)");
   console.log(passwordLength);
 
-
-
-
   if (passwordLength > 7 && passwordLength < 129) {
-    var lowercaseLetters = window.confirm("Do you want to include lowercase?");
-    var upperCase = window.confirm("Do you want to include uppercase?");
+
+    var lowercaseLettersIncl = window.confirm("Do you want to include lowercase?");
+    var upperCaseLettersInl = window.confirm("Do you want to include uppercase?");
     var numbersIncluded = window.confirm("Do you want to include numbers?");
-    var specialCharacters = window.confirm("Do you want to include special characters?");
+    var specialCharactersIncl = window.confirm("Do you want to include special characters?");
+    var charactersSelected = "";
+
+    if (lowercaseLettersIncl) {
+      charactersSelected = charactersSelected + lowercaseLetters;
+    }
+    if (upperCaseLettersInl) {
+      charactersSelected = charactersSelected + upperCase;
+    }
+    if (numbersIncluded) {
+      charactersSelected = charactersSelected + numbers;
+    }
+    if (specialCharactersIncl) {
+      charactersSelected = charactersSelected + specialCharacters;
+    }
 
 
+    for (var i = 0; i < passwordLength; i++) {
+      var index = getRandomInt(charactersSelected.length)
+      generatedPassword = generatedPassword + charactersSelected.charAt(index)
+
+    }
 
   }
+
   else alert("Incorrent number of characters!");
-
+  return generatedPassword;
 }
-
-
-
 
 
 // Write password to the #password input
@@ -50,6 +62,10 @@ function writePassword() {
 
 
 // Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
 
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
