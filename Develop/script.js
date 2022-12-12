35068f1226d36ffb0a349ea8cdeb9310af32e5a4
skullkid4200 <jackeinhorn123@gmail.com>
2022-12-12 14:00:31 -0500

    doing a commit to essentially save what I've done so far.

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,43 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+const result = document.querySelector("#result");
+const includeNumbers = document.querySelector("#numbers");
+const includeSymbols = document.querySelector("#symbols");
+
+var lowercaseLetters = "qwertyuiopasdfghjklzxcvbnm"
+var numbersIncluded = "1234567890"
+var specialCharacters = "~`!@#$%^&*()-_=+':;,<.>/?"
+var upperCase = "QWERTYUIOPASDFGHJKLZXCVBNM"
+var generatedPassword = "";
+
+
+function generatePassword() {
+
+  var generatedPassword = document.querySelector("#password");
+  generatedPassword.value = password;
+
+  var passwordLength = window.prompt("Enter Password Length (Between 8 and 128 Characters)");
+  console.log(passwordLength);
+
+
+
+
+  if (passwordLength > 7 && passwordLength < 129) {
+    var lowercaseLetters = window.confirm("Do you want to include lowercase?");
+    var upperCase = window.confirm("Do you want to include uppercase?");
+    var numbersIncluded = window.confirm("Do you want to include numbers?");
+    var specialCharacters = window.confirm("Do you want to include special characters?");
+
+
+
+  }
+  else alert("Incorrent number of characters!");
+
+}
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -10,5 +48,8 @@ function writePassword() {
 
 }
 
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
